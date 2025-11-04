5ea40ffe406c1ef1b1559fa697e950209aaeb21f
Nicole Morelos Cárdenas <nicmocard@gmail.com>
2025-11-04 17:37:13 -0600

    Update data-source.ts

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -4,15 +4,23 @@ import { Appointment } from "../entities/Appointment"
 import { Credential } from "../entities/Credential"
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./envs"
 
+const isProd = process.env.NODE_ENV === "production";
 export const AppDataSource = new DataSource({
     type: "postgres",
+    ...(process.env.DATABASE_URL
+    ? {
+        url: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
     host: DB_HOST,
     port: Number (DB_PORT),
     username: DB_USER,
     password: DB_PASSWORD,
     database: DB_NAME,
-    synchronize: false, //*ETAPA
-    dropSchema: false, //*DESARROLLO   AL FINALIZAR CAMBIAR A FALSE
+        }),
+    synchronize: !isProd, //*ETAPA
+    dropSchema: false, //*DEV, AL FINALIZAR CAMBIAR A FALSE
     logging: false,
     entities: [User, Appointment, Credential],
     subscribers: [],
